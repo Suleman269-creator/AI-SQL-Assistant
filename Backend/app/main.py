4c1ceb08f724579939ab09dd3d413547737c01b1
Muhammad Suleman Nagri <122889378+Suleman269-creator@users.noreply.github.com>
2026-08-06 13:17:00 +0500

    Changes occur

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title = "AI SQL Assistant API",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Allow all origins for simplicity; adjust as needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
